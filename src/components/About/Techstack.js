8b8ea3c1665554f0185a13e244ddec7c5acb958e
Sushko Andriy <108929517+ItSuhsko14@users.noreply.github.com>
2023-07-22 14:37:11 +0300

    Update Techstack.js change

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -22,15 +22,11 @@ import { TbBrandGolang } from "react-icons/tb";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
+    
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
@@ -52,15 +48,9 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
+  
+    
+     
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
       </Col>

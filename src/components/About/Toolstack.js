61b5ebc167d592bcf3109b7bec19b8a313bf5f06
Sushko Andriy <108929517+ItSuhsko14@users.noreply.github.com>
2023-07-22 15:17:21 +0300

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -11,17 +11,14 @@ import {
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
+     
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
+    />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />

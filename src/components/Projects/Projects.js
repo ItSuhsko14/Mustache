61a5a2a2a288ff13c290bbbe79ddf93cdc13da4a
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-08-12 17:12:28 +0300

    add projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import sicheslavski from "../../Assets/Projects/sicheslavski.png";
+import chef from "../../Assets/Projects/chefassistant.png";
+import allur from "../../Assets/Projects/allur.png";
+import mustdev from "../../Assets/Projects/mustdev.png"
+import kazaki from "../../Assets/Projects/kazaki.png";
+import kozaky from "../../Assets/Projects/kozaky.png";
 
 function Projects() {
   return (
@@ -15,76 +15,75 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          Мої останні <strong className="purple">роботи</strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Ось деякі з проектів, які я виконува.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={mustdev}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Mustdev.pp.ua"
+              description="Лендінг пейдж агенції з веб розробки. Презентація послуг, приклади робіт, ціни. Створено за допомогою React, з деплоєм на Vercel."
+              ghLink="https://github.com/ItSuhsko14/webdevlanding"
+              demoLink="https://www.mustdev.pp.ua/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={kozaky}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Kozaky.com.ua"
+              description="Інформаційний портал, який збирає різноманітну інформацію, каталог послуг, джерел на тему козацтва. Різні частини створено із використанням різного набору технологій таких, як html, css, javascript, php, react, express."
+              ghLink="https://github.com/ItSuhsko14/Kozaky.com.ua"
+              demoLink="http://kozaky.com.ua/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={sicheslavski}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Sicheslavski.com.ua"
+              description="Сайт громадської організації, яка займаєтсья культурно-просвітницької діяльністю. Створений на фреймворку Next.js з використанням статичної генерації"
+              ghLink="https://github.com/ItSuhsko14/sicheslavski-static"
+              demoLink="https://sicheslavski.kozaky.com.ua/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={kazaki}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Kazak.zp.ua"
+              description="Багатосторінковий сайт творчого колектива з презентацією послуг на розгалуженому дереві сторінок, фотогалереєю та блогом. Створений на html, css. Для шаблонізації використовується php."
+              ghLink="https://github.com/ItSuhsko14/kazak"
+              demoLink="https://github.io/kazak"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={allur}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Кінний клуб Аллюр"
+              description="Сайт кінного клуба Аллюр. Презентує кінний клуб, інформацію про основні послуги і контакти. Створено за допомогою препроцессорів pug.js та scss та фреймворку Bootsrap"
+              ghLink="https://github.com/ItSuhsko14/MyAllur"
+              demoLink="http://allur.dp.ua/" 
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={chef}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Chef assistant"
+              description="Односторінковий веб додаток призначений для автоматизації роботи в ресторані або іншому харчовому виробництві. Дозволяє вводити базу технологічних карт і швидко перераховувати калькуляції відповідно до різного об'єму готової продукції. Створено на стеку MERN (MondoDB, Express.js, React, Node.js"
+              ghLink="https://github.com/ItSuhsko14/ChefAssistant"
+              demoLink="https://cheffassistant.pp.ua"      
             />
           </Col>
         </Row>

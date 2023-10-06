a1b1e178d67484df8b70e579e8c70da89af0ed0b
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-10-06 09:08:03 +0300

    change link on github

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/itsuhsko14/Mustache"
+                href="https://github.com/itsuhsko14"
                 target="_blank"
                 className="fork-btn-inner"
               >

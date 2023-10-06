5dd5a6f5fc1c06dec0ec6eb2cf3a1c2e82c0619d
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-10-06 09:18:13 +0300

    correct footer

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -42,9 +42,9 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
+            <h1>Мої соц. мережі</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Завжди відкритий до <span className="purple">спілкування </span>
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">

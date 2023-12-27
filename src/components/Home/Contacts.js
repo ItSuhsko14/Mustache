da8112920c9faf997a850f7a3f70b3013fc5f0da
ItSushko14 <itsushkoandriy@gmail.com>
2023-12-27 17:37:48 +0200

    edit contacts elements

--- src/components/Home/Contacts.js
+++ src/components/Home/Contacts.js
@@ -8,79 +8,71 @@ import {
 import { FaLinkedinIn, FaPhone, FaTelegram, FaEnvelope } from "react-icons/fa";
 
 function Contacts() {
-    return (
-        <Row>
-          <Col md={12}>
-            
-            <ul className="home-about-social-links">
+  return (
+    <Container>
+      <Row>
+        <Col md={12}>
+          <ul className="home-about-social-links d-flex flex-wrap justify-content-center">
+            {/* <li className="social-icons">
+              <a
+                href="tel:+380676309342"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <FaPhone />
+              </a>
+            </li> */}
+
             <li className="social-icons">
-                <a
-                  href="tel:+380676309342"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                    <FaPhone />
-                </a>
-              </li>
+              <a
+                href="https://t.me/alutiy"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <FaTelegram />
+              </a>
+            </li>
 
-              <li className="social-icons">
-                <a
-                  href="https://t.me/alutiy"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                    <FaTelegram />
-                </a>
-              </li>
-              
-              <li className="social-icons">
-                <a
-                  href="mailto:itsushkoandriy@gmail.com"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                    <FaEnvelope />
-                </a>
-              </li>
+            <li className="social-icons">
+              <a
+                href="mailto:itsushkoandriy@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <FaEnvelope />
+              </a>
+            </li>
 
-              <li className="social-icons">
-                <a
-                  href="https://github.com/ItSuhsko14"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/andriysushko/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/mustache.development"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-    )
+            <li className="social-icons">
+              <a
+                href="https://github.com/ItSuhsko14"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/andriysushko/"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Contacts;

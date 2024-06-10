5ef052cffb4830458c55dae7944ba5942ddb0131
ItSushko14 <itsushkoandriy@gmail.com>
2024-06-10 20:37:33 +0300

    new resume

---
+++ src/components/Toolstack.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiVercel,
+  SiJest,
+  SiBootstrap,
+  SiMui
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+     
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJest />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMui />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;

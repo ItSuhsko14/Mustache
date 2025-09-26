1885f7bf8b976abc977b3f20ed65b0c49c6f688a
Sushko Andriy <a.sushko@joinposter.com>
2025-09-26 09:57:53 +0300

    remove ukranian

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -19,11 +19,6 @@ function AboutCard() {
             initiatives.
             <br />
             <br />
-            Я є інженером та магістром із технічною вищою освітою. Більшу частину професійного життя займався створенням
-            проєктів в сфері організації свят, заходів, ресторанному обслуговуванні. Тому майже всі мої проекти у веб
-            програмуванні пов'язані саме з цією доменною сферою.
-            <br />
-            <br />
             Currently, I have the following projects: in the entertainment industry – "Luti"; in the restaurant sector –
             the mobile catering service "Luta Kukhnya"; and I provide web support for the volunteer initiative "Teplo na
             Peredovu".
